117d850cd88b5237682926304c42668bcb66cef5
Shubham <arora.shubham95@gmail.com>
2020-06-26 12:02:18 +0530

    added multile requests

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,38 +2,56 @@
  * 
  */
 
-		/* $("#submit").click(function() {
-			alert("Handler for .click() called.");
-		}); */
+/* $("#submit").click(function() {
+	alert("Handler for .click() called.");
+}); */
 
 $('.ui.dropdown').dropdown();
 
-function getDataFromWebService() {
-$('.card').show();
+function getDataFromWebService(a) {
+	$('.text.wrapper.initial').children().show();
 	let countryLet = $("#country-select").val();
-	
+
 	let categoryLet = $("#category-select").val();
 
 	const vm = new Vue({
-	  el: '#app',
-	  data: {
-	    results: [
-	    ]
-	  },
-	  mounted() {
-		    axios.get('/getNews', {
-    params: {
-      country: countryLet,
-      category: categoryLet
-    }
-  })
-	    .then(response => {this.results = response.data.articles})
-	  }
+		el: a,
+		data: {
+			results: [
+			]
+		},
+		mounted() {
+			axios.get('/getNews', {
+				params: {
+					country: countryLet,
+					category: categoryLet
+				}
+			})
+				.then(response => { this.results = response.data.articles })
+		}
 	});
 }
 
 //getDataFromWebService();
 
-$("#submit").click(function() {
-			getDataFromWebService();
-		});
+$("#submit").click(function () {
+	if($('.card').length < 3) {
+		getDataFromWebService('#app');
+	} else {
+		processForReselection();	
+	}
+});
+
+let count = 1;
+
+function processForReselection() {
+	$('.text.wrapper.initial').children().remove();
+	
+	let currentId = $('.text.wrapper.initial').attr("id");
+	
+	$('.text.wrapper.initial').attr("id","app" + count);
+	$('.text.wrapper.initial').append(document.getElementById('clonableCard').cloneNode(true));
+	getDataFromWebService('#'+"app" + count);
+	count++;
+	
+}
